b8194befb9e73780c060a7f91fab1782092c0a84
wenrouzei <wenrouzei@163.com>
2020-01-31 13:41:38 +0800

    add test

--- gin/main.go
+++ gin/main.go
@@ -3,32 +3,13 @@ package main
 import (
 	"fmt"
 	"path/filepath"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	//router := gin.Default()
 	fmt.Println(filepath.Dir("/home/vagrant"))
-	// Query string parameters are parsed using the existing underlying request object.
-	// The request responds to a url matching:  /welcome?firstname=Jane&lastname=Doe
-	//router.GET("/welcome", func(c *gin.Context) {
-	//	firstname := c.DefaultQuery("firstname", "Guest")
-	//	lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
-	//
-	//	//c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
-	//	//data := make(map[string]string)
-	//	//data["firstname"] = firstname
-	//	//data["lastname"] = lastname
-	//
-	//	json := struct {
-	//		Code      int    `json:"code"`
-	//		Firstname string `json:"firstname"`
-	//		Lastname  string `json:"lastname"`
-	//	}{http.StatusOK, firstname, lastname}
-	//	c.SetCookie("gin", "test", 60, "/", "localhost", true, true)
-	//	c.AsciiJSON(http.StatusOK, json)
-	//})
-	//
-	//router.Run(":8000")
+
 	a := 8 << 2
 	fmt.Println(a)
 
@@ -37,4 +18,28 @@ func main() {
 
 	c = aa << 2 /* 240 = 1111 0000 */ /* 60*2*2 */
 	fmt.Printf("第四行 - c 的值为 %d\n", c)
+
+	router := gin.Default()
+	// Query string parameters are parsed using the existing underlying request object.
+	// The request responds to a url matching:  /welcome?firstname=Jane&lastname=Doe
+	router.GET("/welcome", func(c *gin.Context) {
+		// firstname := c.DefaultQuery("firstname", "Guest")
+		// lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
+
+		c.SetCookie("gin", "test", 60, "/", "localhost", true, true)
+		// c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+		c.JSON(200, gin.H{"status": "you are logged in"})
+		// data := make(map[string]string)
+		// data["firstname"] = firstname
+		// data["lastname"] = lastname
+
+		// json := struct {
+		// 	Code      int    `json:"code"`
+		// 	Firstname string `json:"firstname"`
+		// 	Lastname  string `json:"lastname"`
+		// }{http.StatusOK, firstname, lastname}
+		// c.AsciiJSON(http.StatusOK, json)
+	})
+
+	router.Run(":8000")
 }
--- go.mod
+++ go.mod
@@ -10,21 +10,22 @@ require (
 	github.com/eknkc/amber v0.0.0-20171010120322-cdade1c07385 // indirect
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/flosch/pongo2 v0.0.0-20190707114632-bbf5a6c351f4 // indirect
+	github.com/gin-gonic/gin v1.5.0 // indirect
 	github.com/gorilla/schema v1.1.0 // indirect
 	github.com/hibiken/asynq v0.2.2 // indirect
 	github.com/iris-contrib/blackfriday v2.0.0+incompatible // indirect
 	github.com/iris-contrib/formBinder v5.0.0+incompatible // indirect
 	github.com/iris-contrib/go.uuid v2.0.0+incompatible // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0
 	github.com/kataras/golog v0.0.10 // indirect
-	github.com/kataras/iris v11.1.1+incompatible // indirect
+	github.com/kataras/iris v11.1.1+incompatible
 	github.com/klauspost/compress v1.9.8 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/microcosm-cc/bluemonday v1.0.2 // indirect
 	github.com/ryanuber/columnize v2.1.0+incompatible // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.4.2
-	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa
 	golang.org/x/sys v0.0.0-20200117145432-59e60aa80a0c // indirect
 )
